f4cca8f04f5ab2572b17be1e0abc9f9fffccf0ac
Mhideki78 <mhkanazawa@gmail.com>
2022-02-18 15:21:30 -0300

    modelo Teacher

--- models/Teacher.js
+++ models/Teacher.js
@@ -1,1 +1,22 @@
-ECHO est  ativado.
+const { DataTypes } = require("sequelize");
+const db = require("../db/conn");
+
+const Teacher = db.define("Teacher", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  disciplines: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+module.exports = Teacher;
