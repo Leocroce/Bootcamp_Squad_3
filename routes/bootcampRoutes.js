4cc7de1529dca17bd915df00242569c5a8db410e
Tamiris Rezende Garcia Costa <tamirisrgarcia@gmail.com>
2022-02-21 14:02:00 -0300

    alteração de rota Bootcamp - professor

--- routes/bootcampRoutes.js
+++ routes/bootcampRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const BootcampController = require('../controllers/BootcampController');
 
 router.post('/add', BootcampController.createBootcamp)
-router.get('/:id', BootcampController.showBootcamp)
+router.get('/teacher/:id', BootcampController.showBootcamp)
 router.get('/edit/:id', BootcampController.listUpdateBootcamp)
 router.post('/edit', BootcampController.updateBootcamp)
 router.post('/remove', BootcampController.removeBootcamp)
